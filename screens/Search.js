b2283b791a743f5f51762be5e1304152fc3518da
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2021-12-30 17:03:59 +0530

    Add files via upload

---
+++ screens/Search.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  FlatList
+} from "react-native";
+import { Avatar, ListItem, Icon } from "react-native-elements";
+import db from "../config";
+
+export default class SearchScreen extends Component {
+
+ 
+
+
+ 
+
+  render() {
+    const { searchText, allTransactions } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={styles.upperContainer}>
+          <View style={styles.textinputContainer}>
+           //Search Bar
+
+
+          </View>
+        </View>
+        <View style={styles.lowerContainer}>
+         //Flatlist
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#5653D4"
+  },
+  upperContainer: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textinputContainer: {
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    backgroundColor: "#9DFD24",
+    borderColor: "#FFFFFF"
+  },
+  textinput: {
+    width: "57%",
+    height: 50,
+    padding: 10,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 3,
+    fontSize: 18,
+    backgroundColor: "#5653D4",
+    fontFamily: "Rajdhani_600SemiBold",
+    color: "#FFFFFF"
+  },
+  scanbutton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#9DFD24",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  scanbuttonText: {
+    fontSize: 24,
+    color: "#0A0101",
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  lowerContainer: {
+    flex: 0.8,
+    backgroundColor: "#FFFFFF"
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  subtitle: {
+    fontSize: 16,
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  lowerLeftContaiiner: {
+    alignSelf: "flex-end",
+    marginTop: -40
+  },
+  transactionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  transactionText: {
+    fontSize: 20,
+
+    fontFamily: "Rajdhani_600SemiBold"
+  },
+  date: {
+    fontSize: 12,
+    fontFamily: "Rajdhani_600SemiBold",
+    paddingTop: 5
+  }
+});
